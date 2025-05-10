985db2c944d28846f66fbcdb738e5e275ccf4d3f
Kangjae Choi <awpe89@gmail.com>
2025-05-10 16:51:44 +0900

    feature: add UserEntity, UserDto

---
+++ src/main/java/com/catch_me_study/catch_me_study/domain/user/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.catch_me_study.catch_me_study.domain.user.entity;
+
+import com.catch_me_study.catch_me_study.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "user")
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(insertable = false, updatable = false)
+    private String id;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 80, nullable = false)
+    private String password;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+}
