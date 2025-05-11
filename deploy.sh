f628e6b2417457ebbe6d7e607eca2d8586027a11
Kangjae Choi <awpe89@gmail.com>
2025-05-11 17:48:56 +0900

    chore: add deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Please do not add "remove '.da_data' directory" code
+# If you remove the ".db_data" directory, all DB data is removed.
+
+docker compose down -v # All container down
+docker system prune -a -f # Remove all existing container and volume
+git pull origin master # Pulling code
+docker compose up
+./gradlew clean # Clean-up existing build result
+./gradlew build # Build project
